aecf1f860f5051948937376a8f181e6fdcc4f4ea
Anti-Apple4life <johneverman08@gmail.com>
2022-04-28 11:25:57 -0400

    Finish Section 2

--- Program.cs
+++ Program.cs
@@ -11,18 +11,28 @@ public class Program
 		Console.Write('\n');
 		//sets up restart counter
 		int attempts = 0;
+        int attempts1 = 0;
 		bool done = false;
 		//Sets up restart loop
 		string reciever = "placeholder";
 		string gift = "placeholder";
 		string party = "placeholder";
 		string title = "placeholder";
+        string recognize = "placeholder";
+        string article = "placeholder";
+        string sender = "placeholder";
+        string address = "placeholder";
 		while (!done)
 		{
 			//sets up restart counter (attempts)
 			if (attempts > 1)
 			{
 				Console.Write("You have redone this section " + attempts + " times.");
+				Console.Write('\n');
+			}
+			if (attempts == 1)
+			{
+				Console.Write("You have redone this section " + attempts + " time.");
 				Console.Write('\n');
 			}
 
@@ -68,19 +78,73 @@ public class Program
 					//clears the console and adds 1 to the restart cunter
 					Console.Clear();
 					attempts++;
-					Console.Write("You have redone this section 1 time.");
 					Console.Write('\n');
 					Console.Write('\n');
 					break;
 			//restarts the program
 			}
 		}
+        //Tells the user that they are in Section 2
 		Console.Write("Section 2 \n\n");
 		bool done1 = false;
 		while (!done1)
 		{
-			Console.Write("What do you recognize " + reciever + " as? (e.g: \"aunt\", \"friend\", \"grandfather\" \n");
+            if (attempts1 > 1)
+			{
+				Console.Write("You have redone this section " + attempts1 + " times.");
+				Console.Write('\n');
+			}
+			if (attempts1 == 1)
+			{
+				Console.Write("You have redone this section " + attempts1 + " time.");
+				Console.Write('\n');
+			}
+			Console.Write("What do you recognize " + reciever + " as? (e.g: \"aunt\", \"friend\", \"grandfather\") \n");
 			title = Console.ReadLine();
+            Console.Write('\n');
+            Console.Write('\n');
+            Console.Write("How do you describe " + reciever + " as? (Context: 'answer' " + title + ")\n");
+            recognize = Console.ReadLine();
+            Console.Write('\n');
+            Console.Write('\n');
+            Console.Write("What article do you want to use? (Context: You are 'answer' " + recognize + " " + title + ".)\n");
+            article = Console.ReadLine();
+            Console.Write('\n');
+            Console.Write('\n');
+            Console.Write("How would you like to address " + reciever + "? (e.g: Love, From, Regards)\n");
+            address = Console.ReadLine();
+            Console.Write('\n');
+            Console.Write('\n');
+            Console.Write("What is your name?\n");
+            sender = Console.ReadLine();
+            Console.Write('\n');
+            Console.Write('\n');
+			Console.Write("Here is your letter so far:");
+			Console.Write('\n');
+			Console.Write("Dear " + reciever + "," + '\n' + '\n' + "Thank you so much for coming to my " + party + ". Thank you so much for the " + gift + ". You are " + article + " " + recognize + " " + title + ".\n\n" + address + ", " + sender + ".");
+			Console.Write('\n');
+			Console.Write('\n');
+            Console.Write("Do you like it so far? Type in \"yes\" or \"no\". \nTyping in \"no\" will restart this part of the creation process, and typing in \"yes\" will move you on to the next part of the creation process. \n");
+			string continue2 = Console.ReadLine();
+			Console.Write('\n');
+			//checks the user's response
+			switch (continue2)
+			{
+				case "yes":
+					//stops the restart loop
+					done1 = true;
+					break;
+				case "no":
+					//leaves the restart loop on
+					done1 = false;
+					//clears the console and adds 1 to the restart cunter
+					Console.Clear();
+					attempts1++;
+					Console.Write('\n');
+					Console.Write('\n');
+					break;
+			//restarts the program
+			}
 		}
 	}
 }
